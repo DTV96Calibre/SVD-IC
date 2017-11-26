24f5b26d491fcd9343b5e03f3438b1f532d903ac
Daniel Vasquez <dtv002@bucknell.edu>
2017-11-26 15:51:41 -0500

    SVD calculation implemented

--- main.m
+++ main.m
@@ -1,10 +1,18 @@
 %pkg load 'linear-algebra'
 % Read the image into A as a matrix of uint8
-A = imread('witchhead.jpg');
+[X,map] = imread('witchhead.jpg');
 % Display the image as Figure 1
 %image(A)
 % Subtract 100 from each value in A and store the result in B
-B = A - 100;
+%B = A - 100;
 %C = A.';
 %image(C)
-image(A)
+%image(B)
+Im = X;
+if ~isempty(map)
+    Im = ind2rgb(X,map);
+    [U,S,V] = svd(Im);
+end
+% Convert image from uint8 to doubles for svd
+X = im2double(X);
+[U,S,V] = svd(X(1))
