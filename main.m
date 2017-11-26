80d9b418f5a5e8400b85739d6d2642f571c9e923
Daniel Vasquez <dtv002@bucknell.edu>
2017-11-26 14:28:39 -0500

    Comments changed to start with %

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
-#pkg load 'linear-algebra'
-# Read the image into A as a matrix of uint8
+%pkg load 'linear-algebra'
+% Read the image into A as a matrix of uint8
 A = imread('witchhead.jpg');
-# Display the image as Figure 1
-#image(A)
-# Subtract 100 from each value in A and store the result in B
+% Display the image as Figure 1
+%image(A)
+% Subtract 100 from each value in A and store the result in B
 B = A - 100;
-#C = A.';
-#image(C)
-a = gsvd(A, B)
+%C = A.';
+%image(C)
+image(A)
