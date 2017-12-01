8c4fd7dada6b783192cd0f8e393bc1c711e90c6f
Daniel Vasquez <dtv002@bucknell.edu>
2017-12-01 16:32:41 -0500

    Cleaned up main.m

--- main.m
+++ main.m
@@ -1,21 +1,11 @@
-%pkg load 'linear-algebra'
 % Read the image into A as a matrix of uint8
 [X,map] = imread('witchhead.jpg');
-% Display the image as Figure 1
-%image(A)
-% Subtract 100 from each value in A and store the result in B
-%B = A - 100;
-%C = A.';
-%image(C)
-%image(B)
 Im = X;
-if ~isempty(map)
-    Im = ind2rgb(X,map);
-    [U,S,V] = svd(Im);
-end
+
 % Convert image from uint8 to doubles for svd
 X = im2double(X);
 
+% Seperate 
 [U_r,S_r,V_r] = svd(X(:,:,1));
 [U_g,S_g,V_g] = svd(X(:,:,2));
 [U_b,S_b,V_b] = svd(X(:,:,3));
@@ -97,4 +87,3 @@ difference = rgbImage-X
 image(rgbImage-X)
 
 %-----------------------------
-
